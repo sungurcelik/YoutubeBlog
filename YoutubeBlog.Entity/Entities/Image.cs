f42850e7331f5dac602d40d9d37ac3236ef87955
Salih <Salih@DESKTOP-85QBTPL>
2023-04-29 23:11:15 +0300

    Add project files.

---
+++ YoutubeBlog.Entity/Entities/Image.cs
@@ -0,0 +1,11 @@
+﻿using YoutubeBlog.Core.Entities;
+
+namespace YoutubeBlog.Entity.Entities
+{
+    public class Image : EntityBase, IEntityBase
+    {
+        public string FileName { get; set; }
+        public string FileType { get; set; }
+        public ICollection<Article> Articles { get; set;}
+    }
+}
